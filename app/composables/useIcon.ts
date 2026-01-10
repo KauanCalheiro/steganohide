cf75d162b3fbb684eef899f68733dc6a21beb36a
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-10 10:57:07 +0000

    feat: add icons composable

---
+++ app/composables/useIcon.ts
@@ -0,0 +1,15 @@
+export const useIcon = () => {
+    return {
+        setting: 'heroicons:cog-6-tooth-solid',
+        encrypt: 'heroicons:lock-closed-16-solid',
+        decrypt: 'heroicons:key-16-solid',
+        about: 'heroicons:information-circle-solid',
+        image: 'heroicons:photo-16-solid',
+        download: 'heroicons:arrow-down-on-square-stack-20-solid',
+        upload: 'heroicons:arrow-up-on-square-stack-20-solid',
+        share: 'heroicons:share-16-solid',
+        copy: 'heroicons:document-duplicate-16-solid',
+        success: 'heroicons:check-badge-16-solid',
+        qr_code: 'heroicons:qr-code-16-solid',
+    }
+}
