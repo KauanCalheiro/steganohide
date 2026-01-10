96986ce77d668f1ed50f3c4300bd725c8f994515
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-10 10:57:41 +0000

    feat: add navbar

---
+++ app/components/navbar/index.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+interface NavbarItem {
+  label: string
+  value: string
+  icon: string
+}
+
+const {
+    isMobile
+} = useDevice();
+
+const {
+    encrypt,
+    decrypt,
+    about,
+    setting,
+} = useIcon();
+
+const items = ref<NavbarItem[]>([
+    { label: label('Encrypt'), value: '/encrypt', icon: encrypt },
+    { label: label('Decrypt'), value: '/decrypt', icon: decrypt },
+    { label: label('About'), value: '/about', icon: about },
+    { label: label('Settings'), value: '/setting', icon: setting }
+])
+
+const route = useRoute();
+
+const match = computed<string>((): string  => {
+    const match = items.value.find(i => i.value === route.path);
+    return match ? match.value : '/encrypt';
+});
+
+const value = ref<string>(match.value);
+
+function label(label: string): string {
+    return (computed(() => isMobile.value ? '' : label) as unknown) as string;
+}
+
+function onChange(value: string|number) {
+    navigateTo((value as string));
+}
+</script>
+
+<template>
+    <nav class="fixed inset-x-0 bottom-0 z-50 mb-8 px-6 max-w-xl mx-auto">
+        <UTabs
+            :items="items"
+            v-model:model-value="value"
+            @update:model-value="onChange"
+            class="w-full"
+            size="xl"
+        />
+    </nav>
+</template>
