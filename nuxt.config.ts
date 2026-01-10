ca9798d172d219ea6100093904338b709a8c5343
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-10 14:41:09 +0000

    chore(config): tweak Nuxt UI defaults

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,57 +4,65 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/steganohide/',
-    head: {
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/steganohide/favicon.ico' }
-      ]
-    }
-  },
-
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@vite-pwa/nuxt'
-  ],
-
-  devtools: {
-    enabled: true
-  },
-
-  css: ['~/assets/css/main.css'],
-
-  nitro: {
-    preset: 'static',
-    prerender: {
-      crawlLinks: true
-    }
-  },
-
-  pwa: {
-    registerType: 'autoUpdate',
-    manifestFilename: 'site.webmanifest',
-    injectManifest: {
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    app: {
+        baseURL: '/steganohide/',
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/steganohide/favicon.ico' }
+            ]
+        }
     },
-    devOptions: {
-      enabled: true
+
+    ui: {
+        theme: {
+            defaultVariants: {
+                size: 'xl',
+            },
+        },
+    },
+
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/ui',
+        '@vite-pwa/nuxt'
+    ],
+
+    devtools: {
+        enabled: true
+    },
+
+    css: ['~/assets/css/main.css'],
+
+    nitro: {
+        preset: 'static',
+        prerender: {
+            crawlLinks: true
+        }
+    },
+
+    pwa: {
+        registerType: 'autoUpdate',
+        manifestFilename: 'site.webmanifest',
+        injectManifest: {
+            globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        },
+        devOptions: {
+            enabled: true
+        }
+    },
+
+    routeRules: {
+        '/': { prerender: true }
+    },
+
+    compatibilityDate: '2025-01-15',
+
+    eslint: {
+        config: {
+            stylistic: {
+                commaDangle: 'never',
+                braceStyle: '1tbs'
+            }
+        }
     }
-  },
-
-  routeRules: {
-    '/': { prerender: true }
-  },
-
-  compatibilityDate: '2025-01-15',
-
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
 })
