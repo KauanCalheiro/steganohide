40a622aeb18b35135a7fa6241623c4b73b77799b
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-10 10:57:27 +0000

    feat: add header

---
+++ app/components/header/logo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const {
+    qr_code
+} = useIcon();
+</script>
+
+<template>
+    <div class="inline-flex items-center">
+        <span class="text-3xl font-black">
+            STEGAN
+        </span>
+
+        <UIcon :name="qr_code" class="h-8 w-8 text-3xl" />
+
+        <span class="text-3xl font-black">
+            S
+        </span>
+    </div>
+</template>
