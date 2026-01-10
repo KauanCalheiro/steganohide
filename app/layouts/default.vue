eab474f68c48ff43c0020f3d4b8e78e50918af6e
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-10 16:24:43 +0000

    style

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="min-h-100dvh">
     <Header />
 
-    <main class="mx-auto max-w-3xl w-full h-full px-4 mb-8">
+    <main class="mx-auto max-w-3xl w-full h-full px-4 mb-12">
       <slot />
     </main>
 
