2f4f1e34577717e6f22c071b2725507a22449eda
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-10 12:02:29 +0000

    feat: add default text sizes

---
+++ app/components/app/subtitle.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex flex-row items-center text-xl">
+        <slot />
+    </div>
+</template>
---
+++ app/components/app/title.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex flex-row items-center text-2xl font-bold">
+        <slot />
+    </div>
+</template>
