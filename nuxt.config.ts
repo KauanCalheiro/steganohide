32152ea0077b9ec4a83ed0100fa98fe01fde1326
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-10 16:30:39 +0000

    fix: disable zoom on PWA and mobile devices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,11 @@ export default defineNuxtConfig({
     app: {
         baseURL: '/steganohide/',
         head: {
+            meta: [
+                { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover' },
+                { name: 'apple-mobile-web-app-capable', content: 'yes' },
+                { name: 'mobile-web-app-capable', content: 'yes' }
+            ],
             link: [
                 { rel: 'icon', type: 'image/x-icon', href: '/steganohide/favicon.ico' }
             ]
