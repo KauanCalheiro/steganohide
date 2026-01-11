95700914a01c7349307811d54eee3e8e87482286
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-01-11 11:21:47 +0000

    chore: remove comments from config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 
 import { defineNuxtConfig } from 'nuxt/config'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
         baseURL: '/steganohide/',
